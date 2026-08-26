4e0ef33b57bf1072fea719c55f2931068ae3ef3f
Alex Ballas <alex@ballas.org>
2026-08-27 01:35:44 +0300

    test: add frame path benchmarks

---
+++ internal/driver/glfw/frame_bench_test.go
@@ -0,0 +1,63 @@
+package glfw
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/alexballas/refyne/v2"
+	"github.com/alexballas/refyne/v2/container"
+	"github.com/alexballas/refyne/v2/internal/cache"
+	"github.com/alexballas/refyne/v2/internal/driver/common"
+	"github.com/alexballas/refyne/v2/widget"
+)
+
+// buildTree makes a tree roughly the shape of a real dashboard: tabs, a form of
+// labels/entries and a grid of buttons.
+func buildTree() fyne.CanvasObject {
+	rows := make([]fyne.CanvasObject, 0, 60)
+	for i := 0; i < 60; i++ {
+		rows = append(rows, container.NewHBox(
+			widget.NewLabel(fmt.Sprintf("symbol %d", i)),
+			widget.NewLabel(fmt.Sprintf("%d.%d", i, i)),
+			widget.NewButton("go", func() {}),
+		))
+	}
+	grid := container.NewGridWithColumns(3, rows...)
+	return container.NewAppTabs(
+		container.NewTabItem("one", grid),
+		container.NewTabItem("two", widget.NewLabel("second")),
+	)
+}
+
+func BenchmarkFrameEnsureMinSize(b *testing.B) {
+	cache.BeginFrame()
+	c := newCanvas()
+	c.SetContent(buildTree())
+	c.Resize(fyne.NewSize(1200, 900))
+	c.EnsureMinSize()
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.EnsureMinSize()
+	}
+}
+
+func BenchmarkFrameWalkTrees(b *testing.B) {
+	cache.BeginFrame()
+	c := newCanvas()
+	c.SetContent(buildTree())
+	c.Resize(fyne.NewSize(1200, 900))
+	c.EnsureMinSize()
+
+	count := 0
+	visit := func(*common.RenderCacheNode, fyne.Position) { count++ }
+
+	b.ReportAllocs()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		c.WalkTrees(visit, visit)
+	}
+	b.StopTimer()
+	b.Logf("visited %d nodes per pass", count/b.N/2)
+}
